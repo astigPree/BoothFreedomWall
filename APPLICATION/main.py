d5728d184714f4a4ead54d429be159eb8bedc93c
astigPree <121760681+astigPree@users.noreply.github.com>
2023-08-17 14:09:40 +0800

    done downloading and categories screens

--- APPLICATION/main.py
+++ APPLICATION/main.py
@@ -8,11 +8,24 @@ from kivymd.uix.behaviors import (
     CommonElevationBehavior,
 )
 
+from kivy.uix.modalview import ModalView
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang.builder import Builder
 from kivy.core.text import LabelBase
+from kivy.clock import Clock
 
+class ApplicationSettings(ModalView):
+	pass
+
+class DownloadingView(ModalView):
+	pass
+
+class CategorySelections(ModalView):
+	pass
+
+class Post(MDBoxLayout , CommonElevationBehavior):
+	pass
 
 class PostFeeds(ScrollView):
 	pass
@@ -23,8 +36,17 @@ class CategoryBar(MDBoxLayout, CommonElevationBehavior):
 class CustomToolBar(MDBoxLayout, CommonElevationBehavior):
 	pass
 
+
 class MainWindow(FloatLayout):
-	pass
+	
+	def __init__(self , **kwargs):
+		super(MainWindow , self ).__init__(**kwargs)
+		self.category_selections = CategorySelections()
+		self.downloading_view = DownloadingView()
+	
+	def on_kv_post(self , *args):
+		Clock.schedule_once(self.downloading_view.open , 1)
+		#Clock.schedule_once(self.category_selections.open , 1)
 
 
 class BoothFreedomWallApp(MDApp):
@@ -35,6 +57,8 @@ class BoothFreedomWallApp(MDApp):
 
 LabelBase.register(name = "lato_bold" , fn_regular = "fonts/Lato-Bold.ttf")
 LabelBase.register(name = "lato_bold_italic" , fn_regular = "fonts/Lato-BoldItalic.ttf")
+LabelBase.register(name = "lato_regular" , fn_regular = "fonts/Lato-Regular.ttf")
+LabelBase.register(name = "lato_black_italic" , fn_regular = "fonts/Lato-BlackItalic.ttf")
 
 BoothFreedomWallApp().run()
 
